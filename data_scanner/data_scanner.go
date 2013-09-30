33a10be08eb1d6f4d1453aa0e6af4a16428f1c95
Pierre Baillet <pierre@baillet.name>
2013-09-30 21:47:42 +0200

    Generate all __ folders as bindata

--- data_scanner/data_scanner.go
+++ data_scanner/data_scanner.go
@@ -14,13 +14,20 @@ type DataScanner struct {
 	root string
 }
 
-func (d DataScanner) visit(fullpath string, info os.FileInfo, err error) error {
+func (d DataScanner) visit(relpath string, info os.FileInfo, err error) error {
 	rootAbs, _ := filepath.Abs(d.root)
 
-	baseName := strings.TrimPrefix(fullpath, d.root)
+	fullpath, _ := filepath.Abs(relpath)
+	basename := strings.TrimPrefix(fullpath, rootAbs)
+	binRoot, _ := filepath.Abs(path.Join(d.root, "bindata"))
+
 	// Keep only first level directories
-	if info.IsDir() && len(baseName) > 0 && !strings.Contains(baseName, "/") {
-		fmt.Printf("%s is a directory\n", baseName)
+	if !strings.HasPrefix(basename, "/__") {
+		return nil
+	}
+	fmt.Printf("- %s\n", basename)
+	if info.IsDir() && len(basename) > 0 && !strings.Contains(basename, "/") {
+		fmt.Printf("%s is a directory\n", basename)
 	} else if info.Mode().IsRegular() {
 		in, _ := filepath.Abs(fullpath)
 		fs, err := os.Open(in)
@@ -28,7 +35,8 @@ func (d DataScanner) visit(fullpath string, info os.FileInfo, err error) error {
 			fmt.Fprintf(os.Stderr, "[e] %s\n", err)
 			return err
 		}
-		out, _ := filepath.Abs(path.Join(d.root, "../bindata", baseName+".go"))
+		out := path.Join(binRoot, basename+".go")
+		fmt.Printf("---> %s\n", out)
 
 		// Create missing folder if needed
 		dir, _ := filepath.Split(out)
@@ -43,11 +51,10 @@ func (d DataScanner) visit(fullpath string, info os.FileInfo, err error) error {
 			return err
 		}
 		// Translate binary to Go code.
-		funcname := bindata.SafeFuncname(baseName, "uu")
+		funcname := bindata.SafeFuncname(basename, "uu")
 		bindata.Translate(fs, fd, "uu", funcname, false, false)
 		fs.Close()
-		tocRoot, _ := filepath.Abs(path.Join(d.root, "../bindata"))
-		err = bindata.CreateTOC(tocRoot, "uu")
+		err = bindata.CreateTOC(binRoot, "uu")
 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "[e] %s\n", err)
