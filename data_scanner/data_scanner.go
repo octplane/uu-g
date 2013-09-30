65463d62384f778a45586bf5e3f9c49f114758cf
Pierre Baillet <pierre@baillet.name>
2013-09-30 21:14:23 +0200

    Data Scanner now binifies files

--- data_scanner/data_scanner.go
+++ data_scanner/data_scanner.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/jteeuwen/go-bindata/lib"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 )
@@ -12,11 +14,36 @@ type DataScanner struct {
 	root string
 }
 
-func (d DataScanner) visit(path string, info os.FileInfo, err error) error {
-	baseName := strings.TrimPrefix(path, d.root)
+func (d DataScanner) visit(fullpath string, info os.FileInfo, err error) error {
+	baseName := strings.TrimPrefix(fullpath, d.root)
 	// Keep only first level directories
 	if info.IsDir() && len(baseName) > 0 && !strings.Contains(baseName, "/") {
 		fmt.Printf("%s is a directory\n", baseName)
+	} else if info.Mode().IsRegular() {
+		in, _ := filepath.Abs(fullpath)
+		fs, err := os.Open(in)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "[e] %s\n", err)
+			return err
+		}
+		out, _ := filepath.Abs(path.Join(d.root, "../bindata", baseName))
+
+		// Create missing folder if needed
+		dir, _ := filepath.Split(out)
+		_, err = os.Lstat(dir)
+		if err != nil {
+			os.MkdirAll(dir, 0755)
+		}
+
+		fd, err := os.Create(out)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "[e] %s\n", err)
+			return err
+		}
+		// Translate binary to Go code.
+		bindata.Translate(fs, fd, "uu", bindata.SafeFuncname(baseName, "uu"), false, false)
+		fd.Close()
+		fs.Close()
 	}
 	return nil
 }
